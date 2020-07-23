e431a1f385ec3215a4fa34d587ac8bdcc14299bb
tiafreeman <tiajane.freeman@gmail.com>
2020-07-23 13:00:57 +1000

    added the display console, 2/3 of the height of the container

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
+  display: flex;
+  justify-content: center;
   width: 50vw;
   height: 50vh;
   background-color: white;
@@ -9,6 +11,20 @@ const Container = styled.div`
   margin-left: 25vw;
 `;
 
-const DashboardPage = () => <Container></Container>;
+const DisplayConsole = styled.div`
+  position: relative;
+  max-height: inherit;
+  max-width: inherit;
+  height: 66%;
+  width: 100%;
+  margin: 10px;
+  background-color: red;
+`;
+
+const DashboardPage = () => (
+  <Container>
+    <DisplayConsole />
+  </Container>
+);
 
 export default DashboardPage;
